fc4c686761b08fcc1b7da3affa43c587ec773828
ChinaGaoW <chenghyan28@126.com>
2015-11-07 00:17:47 +0800

    提交原始c代码

---
+++ src/main/java/com/zte/gildedrose/GildedRose.java
@@ -0,0 +1,140 @@
+package com.zte.gildedrose;
+
+/**
+ * Created by Administrator on 2015/11/7 0007.
+ */
+public class GildedRose
+{
+
+}
+
+using System.Collections.Generic;
+
+        namespace GildedRose.Console
+        {
+class Program
+{
+    IList<Item> Items;
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("OMGHAI!");
+
+        var app = new Program()
+        {
+            Items = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                            SellIn = 15,
+                            Quality = 20
+                },
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            }
+
+        };
+
+        app.UpdateQuality();
+
+        System.Console.ReadKey();
+
+    }
+    一般商品 品质下降
+    陈年布里干酪 Aged Brie 品质上升 <50
+
+    一般商品 品质 小于 50
+    萨弗拉斯 Sulfuras 品质不会下降  = 80
+    剧院后台通行证 Backstage passes 品质上升 <10 quality+2 <5 quality+3   0 quality=0
+    public void UpdateQuality()
+    {
+        for (var i = 0; i < Items.Count; i++)
+        {
+            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (Items[i].Quality > 0)
+                {
+                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                    {
+                        Items[i].Quality = Items[i].Quality - 1;
+                    }
+                }
+            }
+            else
+            {
+                if (Items[i].Quality < 50)
+                {
+                    Items[i].Quality = Items[i].Quality + 1;
+
+                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (Items[i].SellIn < 11)
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality = Items[i].Quality + 1;
+                            }
+                        }
+
+                        if (Items[i].SellIn < 6)
+                        {
+                            if (Items[i].Quality < 50)
+                            {
+                                Items[i].Quality = Items[i].Quality + 1;
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+            {
+                Items[i].SellIn = Items[i].SellIn - 1;
+            }
+
+            if (Items[i].SellIn < 0)
+            {
+                if (Items[i].Name != "Aged Brie")
+                {
+                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                    {
+                        if (Items[i].Quality > 0)
+                        {
+                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                            {
+                                Items[i].Quality = Items[i].Quality - 1;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                    }
+                }
+                else
+                {
+                    if (Items[i].Quality < 50)
+                    {
+                        Items[i].Quality = Items[i].Quality + 1;
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+public class Item
+{
+    public string Name { get; set; }
+
+    public int SellIn { get; set; }
+
+    public int Quality { get; set; }
+}
+
+}
+
